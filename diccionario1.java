dffea7fb90029855ee912d9a199602558009f2e5
miguelO9 <160883350+miguelO9@users.noreply.github.com>
2024-04-11 11:53:16 -0500

    Update diccionario1.java

--- diccionario1.java
+++ diccionario1.java
@@ -11,58 +11,118 @@ import java.util.Map;
 public class diccionario1 {
 
     public static void main(String[] args) {
-        //1. Crea un diccionario vacío llamado perro
+          //1. Crea un diccionario vacío llamado perro
+
         Dictionary perro = new Hashtable();
+
         
+
         //2. Añade nombre, color, raza, patas y edad al diccionario perro.
+
        perro.put("nombre", "sacha");
+
        perro.put("color", "negro");
+
        perro.put("raza", "calle");
+
        perro.put("patas", "4");
+
        perro.put("edad", "3 años");
+
        
+
        /*3. Crea un diccionario de estudiante y añade nombre,
+
        apellido, sexo, edad, estado civil, habilidades, país,
+
                ciudad y dirección como claves del diccionario*/
-       Dictionary estudiante = new Hashtable();
+
+       Map<String,String> estudiante = new Hashtable();
+
        ArrayList<String> p = new ArrayList();
+
        p.add("matematicas");
+
        p.add("ajedrez");
+
        
+
        estudiante.put("nombre", "miguel");
+
        estudiante.put("apellido", "oyola");
+
        estudiante.put("sexo", "masculino");
+
        estudiante.put("edad", "20");
-       estudiante.put("estadi civil", "soltero");
-       estudiante.put("habilidades", p);
+
+       estudiante.put("estado_civil", "soltero");
+
+       estudiante.put("habilidades", p.toString());
+
        estudiante.put("pais", "colombia");
+
        estudiante.put("ciudad", "cartagena");
+
        estudiante.put("direccion", "tierra baja");
+
        
+
         
+
        
+
        //4. Obtén la longitud del diccionario del alumno
+
        int longitud = estudiante.size();
+
         System.out.println("la longitud es: " + longitud);
+
        
+
        //5. Obtenga el valor de las habilidades y compruebe el tipo de datos, debe ser una lista
-       String valor = (String) estudiante.get("habilidades");
+
+       System.out.println("Tamano del dict: "+ estudiante.get("habilidades"));
+
        
+
        
+
        //6. Modifique los valores de las habilidades añadiendo una o dos habilidades.
+
        p.add("calculo");
+
        p.add("futbol");
+
+       estudiante.put("habilidades", p.toString());
+
        
+
         //7. Obtener las claves del diccionario como una lista
+
         //8. Obtener los valores del diccionario como una lista
+
         //9. Cambie el diccionario a una lista de tuplas utilizando el método items()
+
         
+
         //10. Eliminar uno de los elementos del diccionario
+
         estudiante.remove("direccion");
+
         System.out.println("los nuevos valores son: " + estudiante );
+
         
+
         //11. Borrar uno de los diccionarios
+
         estudiante.clear();
+
         
+
+        // Tipo de datos en JAVA
+
+        
+
+        System.out.println("Tipo de datos: " +estudiante.getClass().getSimpleName());
         }
 }
